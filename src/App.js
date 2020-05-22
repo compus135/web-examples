6f6ea2eb08f696b412bf60b960919addc5a02fe7
yangsw <yangsw@zetyun.com>
2020-05-22 18:52:24 +0800

    add antd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button } from "antd";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Button type="primary">test</Button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
