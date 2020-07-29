42bd44ab3adcb2ebed45d583ec0cd20dccedfe10
yangsw <yangsw@zetyun.com>
2020-07-29 13:57:10 +0800

    functor

--- src/js/functionalJS/functor/index.js
+++ src/js/functionalJS/functor/index.js
@@ -1,16 +1,54 @@
-const Container = function (val) {
-  this.value = val;
+const { MayBe, map } = require("../utils");
+
+const normalData = {
+  kind: "Listing",
+  data: {
+    modhash: "",
+    children: [
+      {
+        kind: "t1",
+        data: { url: "http://...111", title: "es111" },
+      },
+      {
+        kind: "t2",
+        data: { url: "http://...222", title: "es222" },
+      },
+      {
+        kind: "t3",
+        data: { url: "http://...333", title: "es333" },
+      },
+    ],
+  },
+};
+const emptyData = {
+  kind: "t3",
+};
+const errorData = {
+  message: "something went wrong",
+  errorCode: 2,
+};
+let getTopTenSubRedditPosts = (data) => {
+  let response;
+  try {
+    response = data;
+  } catch (error) {
+    response = errorData;
+  }
+  return response;
 };
 
-Container.of = function (val) {
-  return new Container(val);
+let getTopTenSubRedditData = (type) => {
+  let response = getTopTenSubRedditPosts(type);
+  return MayBe.of(response)
+    .map((arr) => arr["data"])
+    .map((arr) => arr["children"])
+    .map((arr) =>
+      map(arr, (x) => ({ title: x["data"].title, url: x["data"].url }))
+    );
 };
 
-Container.prototype.map = function (fn) {
-  return Container.of(fn(this.value));
-};
+const result = getTopTenSubRedditData(normalData);
+const result2 = getTopTenSubRedditData(emptyData);
+const result3 = getTopTenSubRedditData(errorData);
 
-const instance = Container.of(3);
-const result = instance.map((val) => val * val);
-
-console.log(result);
+console.log(result, result2, result3);
