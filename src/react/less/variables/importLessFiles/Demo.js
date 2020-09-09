0f51eadf1441fa7465a53ab3193180e2708a8518
yangsw <yangsw@zetyun.com>
2020-09-09 11:47:59 +0800

    less

---
+++ src/react/less/variables/importLessFiles/Demo.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./a.less";
+import "./b.less";
+
+const Demo = () => {
+  return <div className="header">test...</div>;
+};
+
+export default Demo;
---
+++ src/react/reactPrinciple/README.md
@@ -0,0 +1,12 @@
+1. react 源码目录结构
+
+```
+{
+  packages: {
+    react: {},
+    "react-dom": {},
+    "react-fetch":{},//演示使用，生产环境不能使用
+    shared:{}
+  },
+};
+```
