71a5c3798d0184a9a551f2dfceb1fd031dfb6afa
compus135 <compus135@163.com>
2021-11-09 18:45:01 +0800

    test

--- src/test/Test.js
+++ src/test/Test.js
@@ -1,38 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-class Test extends React.Component {
-  state = { flag: 0 };
-  handleClick = () => {
-    this.setState((preState) => ({ flag: !preState.flag }));
-  };
-  render() {
-    const { flag } = this.state;
-    return (
-      <div>
-        <button onClick={this.handleClick}>reset</button>
-        <Child flag={flag} />
-      </div>
-    );
-  }
-}
+const Test = () => {
+  return <div>123</div>;
+};
 
-class Child extends React.Component {
-  state = { count: 0 };
-  handleClick = () => {
-    this.setState((preState) => ({ count: preState.count + 1 }));
-  };
-  componentDidUpdate(prevProps, prevState) {
-    prevProps.flag !== this.props.flag && this.setState({ count: 0 });
-  }
-
-  render() {
-    const { count } = this.state;
-    return (
-      <div>
-        {count}
-        <button onClick={this.handleClick}>increase</button>
-      </div>
-    );
-  }
-}
 export default Test;
