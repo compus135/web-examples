ade6d59a09e152f088b9892e4cc70710b4ec4bf3
compus135 <compus135@163.com>
2021-11-16 18:15:30 +0800

    override

--- src/ts/function/override.ts
+++ src/ts/function/override.ts
@@ -1,12 +1,15 @@
+// 根据传入的参数不同返回不同类型的数据
+function fn(arg: string): number;
+function fn(arg: number): { key: string };
 function fn(arg): any {
-  if (typeof arg === "object") {
+  if (typeof arg === "string") {
     return 1;
   } else {
     return { key: 1 };
   }
 }
 
-const r = fn(1);
+const r = fn("");
 console.log(r.key);
 
 export {};
